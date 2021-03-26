16c156ecbaff1952a9c3ba0ac8ad67cf7daa6d39
AmitLevinsonPi <amitlevinson1@gmail.com>
2021-03-26 19:53:06 +0300

    add shell and txt files

---
+++ shell/run_bot.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+Rscript /home/pi/bot/R/retweet_bot.R
+
+date >> /home/pi/bot/txt/run_history.txt
---
+++ shell/update_db.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+Rscript ~/bot/R/bot-db.R
+
+date >> ~/bot/shell/last_db_update.txt
